de1f002449ad2b7b7b95ab7314e6e3cb8976524b
2004yyj <diddbswo04@dgsw.hs.kr>
2020-11-22 00:53:29 +0900

    Update RecyclerService.kt

--- app/src/main/java/com/example/guji/retroFit/RecyclerService.kt
+++ app/src/main/java/com/example/guji/retroFit/RecyclerService.kt
@@ -2,8 +2,13 @@ package com.example.guji.retroFit
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface RecyclerService {
-    @GET("/places")
-    fun getPlaceData() : Call<ArrayList<Model>>
+
+//    @GET("/places")
+//    fun getPlaceData() : Call<ArrayList<Model>>
+
+    @GET("/places/category/{category}")
+    fun getPlaceData(@Path("category") category: String) : Call<ArrayList<Model>>
 }
