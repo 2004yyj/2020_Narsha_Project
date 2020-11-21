d37d59d4bf5b198f05ed29de74db27974968a00f
2004yyj <diddbswo04@dgsw.hs.kr>
2020-11-22 00:53:27 +0900

    Update RecyclerAdapter.kt

--- app/src/main/java/com/example/guji/recyclerView/RecyclerAdapter.kt
+++ app/src/main/java/com/example/guji/recyclerView/RecyclerAdapter.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.guji.R
 import com.example.guji.retroFit.Model
 
-class RecyclerAdapter(private val context: Context?, private val list: ArrayList<Model>) : RecyclerView.Adapter<RecyclerAdapter.Holder>() {
+class RecyclerAdapter(private val context: Context, private val list: ArrayList<Model>) : RecyclerView.Adapter<RecyclerAdapter.Holder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(context).inflate(R.layout.recycler_holder, parent, false)
@@ -17,10 +19,16 @@ class RecyclerAdapter(private val context: Context?, private val list: ArrayList
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
+        val url = "http://guji.c2a.kr"
         val recyclerName = holder.name
         val recyclerCategory = holder.category
+        val recyclerAddress = holder.address
+        val recyclerImage = holder.imageView
+
+        Glide.with(context).load(url + list[position].image_url).circleCrop().into(recyclerImage)
         recyclerName.text = list[position].name
-        recyclerCategory.text = list[position].category
+        recyclerCategory.text = list[position].sub_category
+        recyclerAddress.text = list[position].address
     }
 
     override fun getItemCount(): Int {
@@ -28,7 +36,9 @@ class RecyclerAdapter(private val context: Context?, private val list: ArrayList
     }
 
     open class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imageView: ImageView = itemView.findViewById(R.id.image_view)
         var name: TextView = itemView.findViewById(R.id.name)
         var category: TextView = itemView.findViewById(R.id.category)
+        var address: TextView = itemView.findViewById(R.id.address)
     }
 }
