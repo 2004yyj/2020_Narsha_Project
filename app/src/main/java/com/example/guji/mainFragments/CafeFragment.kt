26ee10db83ca184037be4697638d3c35764c3601
2004yyj <diddbswo04@dgsw.hs.kr>
2020-11-22 00:53:17 +0900

    Update CafeFragment.kt

--- app/src/main/java/com/example/guji/mainFragments/CafeFragment.kt
+++ app/src/main/java/com/example/guji/mainFragments/CafeFragment.kt
@@ -5,13 +5,49 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.guji.R
+import com.example.guji.recyclerView.RecyclerAdapter
+import com.example.guji.retroFit.Model
+import com.example.guji.retroFit.RecyclerClient
+import com.example.guji.retroFit.RecyclerService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CafeFragment : Fragment() {
+
+    val list = ArrayList<Model>()
+    private val retrofit = RecyclerClient.getInstance()
+    private val service: RecyclerService = retrofit.create(RecyclerService::class.java)
+    lateinit var recyclerView : RecyclerView
+    lateinit var recyclerAdapter : RecyclerAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_cafe, container, false)
+        getPlaceList(view)
         return view
+    }
+
+    private fun getPlaceList(view : View) {
+        service.getPlaceData("cafe").enqueue(object : Callback<ArrayList<Model>> {
+            override fun onResponse(call: Call<ArrayList<Model>>, response: Response<ArrayList<Model>>) {
+                if (response.isSuccessful) {
+
+                    list.addAll(response.body()!!)
+
+                    recyclerView = view.findViewById(R.id.cafe_recycler_View)
+                    recyclerAdapter = context?.let { RecyclerAdapter(it, list) }!!
+                    recyclerView.layoutManager = LinearLayoutManager(context)
+                    recyclerView.adapter = recyclerAdapter
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<Model>>, t: Throwable) {
+                t.printStackTrace()
+            }
+        })
     }
 }
