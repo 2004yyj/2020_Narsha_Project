efadb7d7d7efc2ec2a573faae1c5f2cc3358c717
2004yyj <diddbswo04@dgsw.hs.kr>
2020-11-22 13:52:32 +0900

    Create PlaceActivity.kt

---
+++ app/src/main/java/com/example/guji/PlaceActivity.kt
@@ -0,0 +1,62 @@
+package com.example.guji
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.example.guji.retroFit.Model
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.WebMercatorCoord
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import kotlinx.android.synthetic.main.activity_place.*
+import kotlin.properties.Delegates
+
+class PlaceActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mapView : MapView
+    private var latitude : Double = 0.0
+    private var longtitude : Double = 0.0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place)
+
+        val model : Model? = intent.getParcelableExtra("list")
+        position_text.text = model?.name
+        place.text = model?.name
+        sub_category.text = model?.sub_category
+        address.text = model?.address
+
+        latitude = model?.latitude!!
+        longtitude = model.longitude
+
+        setSupportActionBar(place_toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        place_toolbar.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+
+        place_toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        var mapFragment: MapFragment? = supportFragmentManager.findFragmentById(R.id.navermap_fragment) as MapFragment?
+        if(mapFragment == null) {
+            mapFragment = MapFragment.newInstance()
+            supportFragmentManager.beginTransaction().add(R.id.navermap_fragment, mapFragment).commit()
+        }
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        val location = LatLng(latitude, longtitude)
+        val marker = Marker()
+        val cameraPosition = CameraPosition(location, 19.0)
+        naverMap.cameraPosition = cameraPosition
+        marker.position = location
+        marker.map = naverMap
+    }
+}
